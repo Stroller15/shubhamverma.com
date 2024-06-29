11409554d832a9c6642804f2e94a45783bf9e4b7
Stroller15 <shubhamverma6351@gmail.com>
2024-06-29 09:25:28 +0530

    change metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Shubham's Portfolio",
+  title: "Shubham | Fullstack Developer",
   description: "Modern & Minimal Portfolio",
 };
 
